f58d5ed35bae482c46b4e857841f2aeeb4377dbb
vnjl <lsheayenugk1@naver.com>
2022-10-20 17:50:41 +0900

    1020 1750

---
+++ Fragment/app/src/main/java/com/lsheayenugk1/fragment/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.lsheayenugk1.fragment
+
+import androidx.fragment.app.ListFragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lsheayenugk1.fragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setFragment()
+
+        binding.btnSend.setOnClickListener {
+            listFragment.setValue("전달할 값")
+        }
+    }
+
+    lateinit var listFragment: ListFragment
+
+    fun setFragment() {
+        listFragment = ListFragment()
+        val listFragment: ListFragment = ListFragment
+        var bundle = Bundle()
+        bundle.putString("key1", "List Fragment")
+        bundle.putInt("key2", 20170727)
+        listFragment.arguments = bundle
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frameLayout, listFragment)
+        transaction.commit()
+    }
+
+    fun goDetail(){
+        val detailFragment = DetailFragment()
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frameLayout, detailFragment)
+        transaction.addToBackStack("detail")
+        transaction.commit()
+    }
+
+    fun goBack(){
+        onBackPressed()
+    }
+}
---
+++ Fragment_1_3/app/src/main/java/com/lsheayenugk1/fragment_1_3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lsheayenugk1.fragment_1_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
